7fe18d1b5f1b969df109b49ef115c5506ed3c362
Alexandre Debris <debris.alex@gmail.com>
2022-05-21 00:15:08 +0200

    feat: add first interactive items

---
+++ Scripts/Game/ADRP/Utils/ADRP_GatherAction.c
@@ -0,0 +1,25 @@
+class ADRP_GatherAction: ScriptedUserAction {
+
+	[Attribute("", UIWidgets.EditBox, desc: "Display name of what is being gathered")]
+	private string m_GatherItemDisplayName;
+	
+	[Attribute("", UIWidgets.ResourceNamePicker, desc: "Prefab what item is gathered")]
+	private ResourceName m_GatherItemPrefab;
+		
+	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+		auto inventoryManager = SCR_InventoryStorageManagerComponent.Cast(pUserEntity.FindComponent(SCR_InventoryStorageManagerComponent));
+		inventoryManager.TrySpawnPrefabToStorage(m_GatherItemPrefab);
+	}
+	
+	override bool GetActionNameScript(out string outName)
+	{
+		outName = string.Format("Gather %1", m_GatherItemDisplayName);
+		return true;
+	}
+	
+	override bool CanBePerformedScript(IEntity user)
+ 	{
+		return true;
+ 	}
+}
