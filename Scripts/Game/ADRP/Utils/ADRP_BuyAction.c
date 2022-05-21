f0f701a1a17c5f2fe9e4edcec10449fd7410a200
Zomblard <leo.goigoux@gmail.com>
2022-05-21 18:06:25 +0200

    feat(items): Add Water Pizza Copper Key

---
+++ Scripts/Game/ADRP/Utils/ADRP_BuyAction.c
@@ -0,0 +1,25 @@
+class ADRP_BuyAction: ScriptedUserAction {
+
+	[Attribute("", UIWidgets.EditBox, desc: "Display name of what is being buy")]
+	private string m_BuyItemDisplayName;
+	
+	[Attribute("", UIWidgets.ResourceNamePicker, desc: "Prefab what item is buy")]
+	private ResourceName m_BuyItemPrefab;
+		
+	override void PerformAction(IEntity pOwnerEntity, IEntity pUserEntity)
+	{
+		auto inventoryManager = SCR_InventoryStorageManagerComponent.Cast(pUserEntity.FindComponent(SCR_InventoryStorageManagerComponent));
+		inventoryManager.TrySpawnPrefabToStorage(m_BuyItemPrefab);
+	}
+	
+	override bool GetActionNameScript(out string outName)
+	{
+		outName = string.Format("Buy %1", m_BuyItemDisplayName);
+		return true;
+	}
+	
+	override bool CanBePerformedScript(IEntity user)
+ 	{
+		return true;
+ 	}
+}
---
+++ Scripts/Game/Map/ComponentsUI/SCR_WhereAmIUI.c
@@ -0,0 +1,79 @@
+class SCR_WhereAmIUI : SCR_MapUIBaseComponent
+{
+	protected Widget m_widget;
+	protected SCR_MapToolEntry m_ToolMenuEntry;
+
+	override void Update()
+	{
+		if (!m_widget)
+			return;
+
+		ChimeraCharacter player = ChimeraCharacter.Cast(SCR_PlayerController.GetLocalControlledEntity());
+		if (!player)
+		{
+			m_widget.RemoveFromHierarchy();
+			return;
+		}
+
+		vector posPlayer = player.GetOrigin();
+
+		float x, y;
+		m_MapEntity.WorldToScreen(posPlayer[0], posPlayer[2], x, y, true);
+
+		x = GetGame().GetWorkspace().DPIUnscale(x);
+		y = GetGame().GetWorkspace().DPIUnscale(y);
+
+		FrameSlot.SetPos(m_widget, x, y);
+	}
+
+	override void OnMapOpen(MapConfiguration config)
+	{
+		super.OnMapOpen(config);
+
+		if (m_widget)
+		{
+			return;
+		}
+
+		m_widget = GetGame().GetWorkspace().CreateWidgets("{3513CA0C0C83D2E3}UI/layouts/GPS/PlayerMapIndicator.layout", m_RootWidget);
+
+		ResourceName imageSet = "{5968FAE6F3EC3B1D}UI/Textures/GPS/marker.imageset";
+		ImageWidget image = ImageWidget.Cast(m_widget.FindAnyWidget("Image"));
+		if(image)
+		{
+			image.LoadImageFromSet(0, imageSet, "PlayerMarker");
+			image.SetColor(Color.FromRGBA(180, 0, 0, 255));
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	override void Init()
+	{
+		SCR_MapToolMenuModule toolMenu = SCR_MapToolMenuModule.Cast(m_MapEntity.GetMapModule(SCR_MapToolMenuModule));
+		if (toolMenu)
+		{
+			m_ToolMenuEntry = toolMenu.RegisterToolMenuEntry("{5968FAE6F3EC3B1D}UI/Textures/GPS/marker.imageset", "PlayerMarker", false, 99);
+			m_ToolMenuEntry.m_OnClick.Insert(ZoomInOnPlayer);
+		}
+	}
+
+	//------------------------------------------------------------------------------------------------
+	protected void ZoomInOnPlayer()
+	{
+		// Reset button color so that it doesn't behave like a toggle
+		m_ToolMenuEntry.SetColor(Color.Gray25);
+
+		ChimeraCharacter player = ChimeraCharacter.Cast(SCR_PlayerController.GetLocalControlledEntity());
+		if (!player)
+		{
+			return;
+		}
+
+		vector posPlayer = player.GetOrigin();
+
+		vector mapSize = m_MapEntity.GetMapWidget().GetSizeInUnits();
+		float zoomVal = mapSize[0] / (mapSize[0] * (m_MapEntity.GetMapWidget().PixelPerUnit() * 0.5));
+
+		m_MapEntity.ZoomPanSmooth(zoomVal,posPlayer[0],posPlayer[2]);
+	}
+};
